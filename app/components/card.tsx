033dfcf3ecab4c7622b82c686ff8a0f63727e9d8
Johnathan <johnathan.herber47@gmail.com>
2024-06-24 17:56:03 -0400

    Icon Component

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -15,38 +15,41 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ ativo, ordemProducao, recursoBin, tara, pesoProduto, turno, pesagemType, operador, data }) => {
+
+    const iconName = pesagemType === "Pesagem Direta" ? "close_fullscreen" : "precision_manufacturing";
+
     return (
-        <div className="bg-[#5E95AA] w-80 h-90 shadow-md rounded-lg p-10 pb-10">
-            <header>
-                <h2 className="text-xl font-bold mb-2">{ativo}</h2>
-            </header>
+        <div className="bg-[#5E95AA] w-80 h-80 shadow-md rounded-lg p-10 pb-10">
+            <div>
+                <h2 className="text-lg font-bold mb-1">{ativo}</h2>
+            </div>
 
             <div className="border-b-2 p-1 mb-3"></div>
             <main>
                 <div className="flex">
                     <p className="mr-[150px]">OP</p>
-                    <p className="text-gray-50 font-bold mb-1">{ordemProducao}</p>
+                    <p className="text-gray-50  mb-1">{ordemProducao}</p>
                 </div>
                 <div>
-                    <p className="text-gray-50 font-bold mb-1">Recurso Bin: {recursoBin}</p>
+                    <p className="text-gray-50 mb-1">Recurso Bin: {recursoBin}</p>
                 </div>
 
 
 
-                <p className="text-gray-50 font-bold mb-1">Tara: {tara} kg</p>
+                <p className="text-gray-50 mb-1">Tara: {tara} kg</p>
 
-                <p className="text-gray-50 font-bold mb-1">Peso: {pesoProduto} kg</p>
+                <p className="text-gray-50 mb-1">Peso: {pesoProduto} kg</p>
 
-                <p className="text-gray-50 font-bold mb-1">Turno: {turno}</p>
+                <p className="text-gray-50 mb-1">Turno: {turno}</p>
 
-                <p className="text-gray-50 font-bold mb-1">Operador: {operador}</p>
+                <p className="text-gray-50 mb-1">Operador: {operador}</p>
 
-                <p className="text-gray-50 font-bold">{data}</p>
+                <p className="text-gray-50">{data}</p>
             </main>
 
 
             <div className=" border-t-2 flex">
-                <Icon icon={"close_fullscreen"} className="mr-2 mt-2" /><p className="mt-2">{pesagemType}</p>
+                <Icon icon={iconName} className="mr-2 mt-2" /><p className="mt-2">{pesagemType}</p>
             </div>
         </div>
     );
